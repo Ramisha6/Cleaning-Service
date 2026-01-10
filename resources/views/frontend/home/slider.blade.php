b7cacbad93b63bb13a84eea8697cd2516d973f4b
Abdulla Al Faruq <abdullaalfaruq103694@gmail.com>
2026-01-10 19:32:27 +0600

    slider issue fixed

--- resources/views/frontend/home/slider.blade.php
+++ resources/views/frontend/home/slider.blade.php
@@ -1,33 +1,35 @@
-<section class="">
+<section>
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+
+                <div class="swiper bannerSwiper">
+                    <div class="swiper-wrapper">
+
+                        @php
+                            $banners = [
+                                'frontend/assets/img/banner/banner_3.jpg',
+                                'frontend/assets/img/banner/banner_2.jpg',
+                                'frontend/assets/img/banner/banner_1.jpg',
+                            ];
+                        @endphp
+
+                        @foreach($banners as $banner)
+                            <div class="swiper-slide">
+                                <img src="{{ asset($banner) }}" class="d-block w-100" alt="Banner">
+                            </div>
+                        @endforeach
+
                     </div>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="{{ asset('frontend/assets/img/banner/banner_3.jpg') }}" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{ asset('frontend/assets/img/banner/banner_2.jpg') }}" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="{{ asset('frontend/assets/img/banner/banner_1.jpg') }}" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+
+                    <!-- Indicators -->
+                    <div class="swiper-pagination"></div>
+
+                    <!-- Navigation -->
+                    <div class="swiper-button-next"></div>
+                    <div class="swiper-button-prev"></div>
                 </div>
+
             </div>
         </div>
     </div>
